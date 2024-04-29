45db55f4fd0a59cc8ec64ef181e56f0e56ed63fe
Eric <ericlzg@umich.edu>
2024-04-29 19:39:50 -0400

    more

--- eric.py
+++ eric.py
@@ -13,11 +13,25 @@ def writedb(data,db):
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path + "/" + db)
     cur = conn.cursor() 
+    cur.execute("CREATE TABLE IF NOT EXISTS Tracts (id INTEGER PRIMARY KEY, name TEXT UNIQUE, income INTEGER, fips_code INTEGER)")
+    count = 0
     for i in range(1,len(data)):
+        if count >= 25:
+            conn.commit()
+            return "Terminating after reaching write limit"
         tract = data[i][0]
         tractname = tract[0: tract.find(";")]
-        medianincome = data[i][1]
-        fips = data[i][4]
+        medianincome = int(data[i][1])
+        if medianincome < 0:
+            medianincome = None
+        fips = int(data[i][4])
+        cur.execute("INSERT OR IGNORE INTO Tracts (id, name, income, fips_code) VALUES (?,?,?,?)",(i, tractname, medianincome, fips))
+        if cur.lastrowid != 0:
+            count += 1
+    return "All data written"
+        
+
+
 
 
 
